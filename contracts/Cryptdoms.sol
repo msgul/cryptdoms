fba8c95ff61753465994a5dea9886a42910873e8
etkin <etknpnar@gmail.com>
2021-09-02 08:58:23 +0300

    Events for logging added

--- contracts/Cryptdoms.sol
+++ contracts/Cryptdoms.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Cryptdoms {
     
 
-    struct kingdom{
+    struct Kingdom{
         string kingdomName;
         uint8 r;
         uint8 g;
@@ -17,13 +17,17 @@ contract Cryptdoms {
     uint8 constant landSize = 10;
     
     address[landSize ** 2] ownedBy;
-    mapping (address => kingdom) ownerToKingdom;
+    mapping (address => Kingdom) ownerToKingdom;
     uint randNonce = 0;
 
     /* 
      * Consideration - Should it be able to receive ETH? 
      * This contract currently unable to receive ETH through send
      */ 
+    
+    event Battle(address indexed _attacker, address indexed _defender, bool _success);
+    event KingdomCreation(address indexed _owner, Kingdom _kingdom);
+    event LandBought(address indexed _owner, uint _landId);
 
     constructor() {
         owner = msg.sender;
@@ -63,26 +67,36 @@ contract Cryptdoms {
         require(ownedBy[landId] == address(0), "You can only buy unowned lands!");
         
         if (ownerToKingdom[msg.sender].isCreated == 0){
+            // could be a seperate function createKingdom()?
             ownerToKingdom[msg.sender].isCreated = 1;
             ownerToKingdom[msg.sender].kingdomName = kingdomName;
             ownerToKingdom[msg.sender].r = r;
             ownerToKingdom[msg.sender].g = g;
             ownerToKingdom[msg.sender].b = b;
+            emit KingdomCreation(msg.sender, ownerToKingdom[msg.sender]);
         }
         
+        emit LandBought(msg.sender, landId);
         ownedBy[landId] = msg.sender;
     }
 
-    function attackLand(uint myLandId, uint landId) public landOwner(myLandId) {
+    function attackLand(uint myLandId, uint landId) public landOwner(myLandId) returns(bool){
         require(myLandId < landSize ** 2 && landId < landSize ** 2, "Please provide legit land id!");
         require(_isNeighbour(myLandId, landId), "You can only attack your neighbour lands!");
         address enemy = ownedBy[landId];
         uint result = randMod(100);
-        // TODO event emit for win or lose and return result text
-        if(result > 45)
+        bool success;
+        if(result > 45){
             ownedBy[landId] = msg.sender;
-        else
+            success = true;
+        }
+        else{
             ownedBy[myLandId] = enemy;
+            success = false;
+        }
+        emit Battle(msg.sender, enemy, success);
+        
+        return success;
     }
 
     // Do we want to withdraw only a portion of the balance or all of it?
@@ -102,13 +116,13 @@ contract Cryptdoms {
 
     /* ----- View Functions ----- */
 
-    function viewMap() public view returns(address[] memory, kingdom[] memory) {
+    function viewMap() public view returns(address[] memory, Kingdom[] memory) {
         address[] memory addressMap = new address[](landSize ** 2);
         for(uint i=0; i < landSize ** 2; i++) {
             addressMap[i] = ownedBy[i];
         }
 
-        kingdom[] memory kingdomMap = new kingdom[](landSize ** 2);
+        Kingdom[] memory kingdomMap = new Kingdom[](landSize ** 2);
         for(uint i=0; i < landSize ** 2; i++) {
             kingdomMap[i] = ownerToKingdom[ownedBy[i]];
             delete(kingdomMap[i].balance);
@@ -136,19 +150,14 @@ contract Cryptdoms {
         return lands;
     }
 
-    function getKingdom() public view returns(kingdom memory) {
+    function getKingdom() public view returns(Kingdom memory) {
         return ownerToKingdom[msg.sender];
     }
 
-    // Will probably be removed
-    function getKingdomByAddress(address _address) public view returns(kingdom memory) {
-        kingdom memory userKingdom = ownerToKingdom[_address];
-        return userKingdom;
-    }
-
-    function getKingdomByLand(uint landId) public view returns(kingdom memory) {
-        kingdom memory userKingdom = ownerToKingdom[ownedBy[landId]];
+    function getKingdomByAddress(address _address) public view returns(Kingdom memory) {
+        Kingdom memory userKingdom = ownerToKingdom[_address];
         delete(userKingdom.balance);
         return userKingdom;
     }
+
 }
